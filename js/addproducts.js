ff338d572025cf5ffdb10b20d40c9f947997a861
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2022-11-20 23:49:10 +0100

    sggb

--- js/addproducts.js
+++ js/addproducts.js
@@ -1,9 +1,9 @@
 const jacketsContainer = document.querySelector(".product-list");
 
 for(var i=0; i < products.length; i++){
-    let productLink = '<a href="product.html?id=${products[i].id}" class="cta cta-small">View</a>;';
+    let productLink = `<a href="product.html?id=${products[i].id}" class="cta cta-small">View</a>`;
     if(products[i].sales === true){
-        productLink = <a href="product.html?id=${products[i].id}" class="cta cta-small cta-sale">On Sale</a>;
+        productLink = `<a href="product.html?id=${products[i].id}" class="cta cta-small cta-sale">On Sale</a>`;
     }
     jacketsContainer.innerHTML += `<li class="products">
     <img src="${products[i].image}" alt="${products[i].name}" />
