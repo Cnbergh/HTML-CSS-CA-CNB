32b2b7781e6f572844d57e3c2e22a72104e40bc2
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2022-11-20 16:45:38 +0100

    dfbg

--- js/details.js
+++ js/details.js
@@ -1,26 +1,15 @@
+details.js: 
+
 const queryString=document.location.search;
-const params=newURLSearchParams(queryString);
-const productId=params.get("id");
+let url = new URL(document.location);
+let searchParams = new URLSearchParams(url.search);
+const productId= searchParams.get('id')
 const productContainer=document.querySelector(".product-specific");
 
-constproduct=products.find(({id })=>id==productId);
+const product=products.find(({id })=>id==productId);
 
 productContainer.innerHTML=`<section>
 <img src="${product.image}" alt="${product.name}" class="product-image" />
-<div class="product-image_thumbnails">
-  <div>
-    <img src="../images/product-specific.jpg" alt="product3" class="product-image" />
-  </div>
-  <div>
-    <img src="../images/product-specific.jpg" alt="product3" class="product-image" />
-  </div>
-  <div>
-    <img src="../images/product-specific.jpg" alt="product3" class="product-image" />
-  </div>
-  <div>
-    <img src="../images/product-specific.jpg" alt="product3" class="product-image" />
-  </div>
-</div>
 </section>
 <section class="product-specific__details">
 <h1>${product.name}</h1>
--- js/products.js
+++ js/products.js
@@ -1,9 +1,8 @@
-
 const products=[
     {
         name: "RainyDays Jacket",
         price: 425,
-        image: "../images/products/product1.jpg",
+        image: "images/products/product1.jpg",
         id: 1,
         description: "Det er ingen som elsker smerte i seg selv, som søker og ønsker det, helt enkelt fordi det er smerte...",
         featured: true,
@@ -12,7 +11,7 @@ const products=[
     {
         name: "RainyDays Jacket2",
         price: 750,
-        image: "../images/products/product2.jpg",
+        image: "images/products/product2.jpg",
         id: 2,
         description: "Det er ingen som elsker smerte i seg selv, som søker og ønsker det, helt enkelt fordi det er smerte...",
         featured: false,
@@ -21,7 +20,7 @@ const products=[
     {
         name: "RainyDays Jacket3",
         price: 625,
-        image: "../images/products/product3.jpg",
+        image: "images/products/product3.jpg",
         id: 3,
         description: "Det er ingen som elsker smerte i seg selv, som søker og ønsker det, helt enkelt fordi det er smerte...",
         featured: false,
@@ -30,7 +29,7 @@ const products=[
     {
         name: "RainyDays Jacket4",
         price: 525,
-        image: "../images/products/product4.jpg",
+        image: "images/products/product4.jpg",
         id: 4,
         description: "Det er ingen som elsker smerte i seg selv, som søker og ønsker det, helt enkelt fordi det er smerte...",
         featured: true,
@@ -39,7 +38,7 @@ const products=[
     {
         name: "RainyDays Jacket5",
         price: 450,
-        image: "../images/products/product5.jpg",
+        image: "images/products/product5.jpg",
         id: 5,
         description: "Det er ingen som elsker smerte i seg selv, som søker og ønsker det, helt enkelt fordi det er smerte...",
         featured: true,
@@ -48,7 +47,7 @@ const products=[
     {
         name: "RainyDays Jacket6",
         price: 675,
-        image: "../images/products/product6.jpg",
+        image: "images/products/product6.jpg",
         id: 6,
         description: "Det er ingen som elsker smerte i seg selv, som søker og ønsker det, helt enkelt fordi det er smerte...",
         sales: true
@@ -56,7 +55,7 @@ const products=[
     {
         name: "RainyDays Jacket7",
         price: 680,
-        image: "../images/products/product7.jpg",
+        image: "images/products/product7.jpg",
         id: 7,
         description: "Det er ingen som elsker smerte i seg selv, som søker og ønsker det, helt enkelt fordi det er smerte...",
         featured: true,
@@ -65,7 +64,7 @@ const products=[
     {
     name: "RainyDays Jacket8",
     price: 620,
-    image: "../images/products/product8.jpg",
+    image: "images/products/product8.jpg",
     id: 8,
     description: "Det er ingen som elsker smerte i seg selv, som søker og ønsker det, helt enkelt fordi det er smerte...",
     featured: true,
@@ -74,7 +73,7 @@ const products=[
 {
     name: "RainyDays Jacket9",
     price: 250,
-    image: "../images/products/product9.jpg",
+    image: "images/products/product9.jpg",
     id: 9,
     description:  "Det er ingen som elsker smerte i seg selv, som søker og ønsker det, helt enkelt fordi det er smerte...",
     featured: false
