f21cceb5204b766cf5835045452a9a323d9ea399
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2023-03-22 11:47:27 +0100

    Clean up

--- js/details.js
+++ js/details.js
@@ -1,12 +1,12 @@
-const queryString=document.location.search;
+const queryString = document.location.search;
 let url = new URL(document.location);
 let searchParams = new URLSearchParams(url.search);
-const productId= searchParams.get('id')
-const productContainer=document.querySelector(".product-specific");
+const productId = searchParams.get("id");
+const productContainer = document.querySelector(".product-specific");
 
-const product=products.find(({id })=>id==productId);
+const product = products.find(({ id }) => id == productId);
 
-productContainer.innerHTML=`<section>
+productContainer.innerHTML = `<section>
 <img src="${product.image}" alt="${product.name}" class="product-image" />
 </section>
 <section class="product-specific__details">
@@ -14,4 +14,4 @@ productContainer.innerHTML=`<section>
 ${product.description}
 <p class="product-specific__price">${product.price}</p>
 <a href="../contact.html" class="cta cta-large">Contact Us</a>
-</section>`
+</section>`;
